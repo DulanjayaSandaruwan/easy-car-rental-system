9e9f416d7209759736e726a715ea43b4603d2a07
DulanjayaSandaruwan <dulanjayasandaruwan1998@gmail.com>
2022-07-16 02:39:39 +0530

    add app bar

--- easy-car-rental-system-front-end/src/Components/Header/Navbar.jsx
+++ easy-car-rental-system-front-end/src/Components/Header/Navbar.jsx
@@ -1,52 +1,28 @@
 import React from 'react';
 import {AppBar, Box, Button, makeStyles, Menu, MenuItem, Toolbar, Typography} from "@material-ui/core";
+import Profile from "./Navtabs/Profile";
 
 const useStyles = makeStyles((theme) => ({
+    toolbar: {
+        display: "flex",
+        flexFlow: "row wrap",
+        justifyContent: "space-between",
+    },
     logo: {
         color: "white",
     }
 }))
 
 export default function Navbar() {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
     const classes = useStyles()
     return (
         <AppBar position="static">
-            <Toolbar>
+            <Toolbar className={classes.toolbar}>
                 <Typography variant="h6" className={classes.logo}>
                     Admin Dashboard
                 </Typography>
                 <Box>
-                    <Button
-                        id="basic-button"
-                        aria-controls={open ? 'basic-menu' : undefined}
-                        aria-haspopup="true"
-                        aria-expanded={open ? 'true' : undefined}
-                        onClick={handleClick}
-                    >
-                        Dashboard
-                    </Button>
-
-                    <Menu
-                        id="basic-menu"
-                        anchorEl={anchorEl}
-                        open={open}
-                        onClose={handleClose}
-                        MenuListProps={{
-                            'aria-labelledby': 'basic-button',
-                        }}
-                    >
-                        <MenuItem onClick={handleClose}>Profile</MenuItem>
-                        <MenuItem onClick={handleClose}>My account</MenuItem>
-                        <MenuItem onClick={handleClose}>Logout</MenuItem>
-                    </Menu>
+                    <Profile/>
                 </Box>
             </Toolbar>
         </AppBar>
---
+++ easy-car-rental-system-front-end/src/Components/Header/Navtabs/Profile.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import {Avatar, Box, Button, ListItem, ListItemIcon, ListItemText, Menu, MenuItem} from "@material-ui/core";
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+export default function Profile() {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    const dropDownData = [{label: "settings", icon: <SettingsIcon/>}, {label: "Logout", icon: <LogoutIcon/>},];
+
+    return (<Box>
+        <Button
+            id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+            startIcon={<Avatar
+                src="https://media-exp2.licdn.com/dms/image/C4D03AQEpdlQCLhvzzA/profile-displayphoto-shrink_800_800/0/1656002497819?e=1663200000&v=beta&t=HLl4pA8fpefmdx1ToFEBlbbOF2ptHIe3D6-XPxx-9qM"></Avatar>}
+        >
+        </Button>
+
+        <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+                'aria-labelledby': 'basic-button',
+            }}
+        >
+            {dropDownData.map((item, i) => (<MenuItem key={i} component={ListItem} onClick={handleClose}>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText>{item.label}</ListItemText>
+            </MenuItem>))}
+
+        </Menu>
+    </Box>)
+}

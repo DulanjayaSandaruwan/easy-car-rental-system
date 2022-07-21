c3f2e61f56cd2ada0cd831a03b1aeaf9aabddd65
DulanjayaSandaruwan <dulanjayasandaruwan1998@gmail.com>
2022-07-21 19:36:57 +0530

    end of admin profile

---
+++ easy-car-rental-system-front-end/src/js/admin/admin.js
@@ -0,0 +1,206 @@
+$('#btn_mainPage').click(function () {
+    $('#admin_Home_Page').css({
+        'display': 'block'
+    });
+    $('#admin_profile_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Customer_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Car_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Driver_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Order_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Payment_Page').css({
+        'display': 'none'
+    });
+    getCustomerCount();
+    getCarCount();
+    getDriverCount();
+    getOrderCount();
+});
+
+$('#btn_ProfilePage').click(function () {
+    $('#admin_Home_Page').css({
+        'display': 'none'
+    });
+    $('#admin_profile_Page').css({
+        'display': 'block'
+    });
+    $('#admin_Customer_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Car_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Driver_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Order_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Payment_Page').css({
+        'display': 'none'
+    });
+    getAdmin();
+});
+
+$('#btn_CustomerPage').click(function () {
+    $('#admin_Home_Page').css({
+        'display': 'none'
+    });
+    $('#admin_profile_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Customer_Page').css({
+        'display': 'block'
+    });
+    $('#admin_Car_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Driver_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Order_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Payment_Page').css({
+        'display': 'none'
+    });
+    loadAllCustomer();
+    getLastCustomerId();
+    // getCustomerDLIC();
+});
+
+$('#btn_CarPage').click(function () {
+    $('#admin_Home_Page').css({
+        'display': 'none'
+    });
+    $('#admin_profile_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Customer_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Car_Page').css({
+        'display': 'block'
+    });
+    $('#admin_Driver_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Order_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Payment_Page').css({
+        'display': 'none'
+    });
+    loadAllCars();
+    getLastCarId();
+    getCarRegNo();
+
+});
+
+$('#btn_DriverPage').click(function () {
+    $('#admin_Home_Page').css({
+        'display': 'none'
+    });
+    $('#admin_profile_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Customer_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Car_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Driver_Page').css({
+        'display': 'block'
+    });
+    $('#admin_Order_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Payment_Page').css({
+        'display': 'none'
+    });
+    loadAllDriver();
+    getLastDriverId();
+});
+
+$('#btn_OrderPage').click(function () {
+    $('#admin_Home_Page').css({
+        'display': 'none'
+    });
+    $('#admin_profile_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Customer_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Car_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Driver_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Order_Page').css({
+        'display': 'block'
+    });
+    $('#admin_Payment_Page').css({
+        'display': 'none'
+    });
+    loadAllOrders();
+});
+
+$('#btn_PaymentPage').click(function () {
+    $('#admin_Home_Page').css({
+        'display': 'none'
+    });
+    $('#admin_profile_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Customer_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Car_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Driver_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Order_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Payment_Page').css({
+        'display': 'block'
+    });
+});
+
+$('#btnAdminProfile').click(function () {
+    $('#admin_Home_Page').css({
+        'display': 'none'
+    });
+    $('#admin_profile_Page').css({
+        'display': 'block'
+    });
+    $('#admin_Customer_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Car_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Driver_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Order_Page').css({
+        'display': 'none'
+    });
+    $('#admin_Payment_Page').css({
+        'display': 'none'
+    });
+});
